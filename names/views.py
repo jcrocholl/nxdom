2f02f68698fe8379086b59f03503da744ad2e2d0
Johann C. Rocholl <johann@rocholl.net>
2009-10-24 10:27:06 -0700

    Added text file upload.

--- names/views.py
+++ names/views.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 from django import forms
 from django.http import HttpResponseRedirect
@@ -9,19 +10,38 @@ from ragendja.dbutils import get_object_or_404
 from names.models import Idea
 
 
-class CreateNamesForm(forms.Form):
+class EnterNamesForm(forms.Form):
     names = forms.CharField(
         max_length=10000,
         widget=forms.TextInput(attrs={'class': 'text span-17 focus'}))
 
 
+class UploadNamesForm(forms.Form):
+    upload = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'upload'}))
+
+
 def index(request):
-    create_names_form = CreateNamesForm(request.POST or None)
-    if create_names_form.is_valid():
-        names = create_names_form.cleaned_data['names'].split()
+    # Save newly entered name ideas.
+    enter_names_form = EnterNamesForm(
+        request.POST if 'submit_names' in request.POST else None)
+    if enter_names_form.is_valid():
+        names = enter_names_form.cleaned_data['names'].split()
         return create_ideas(request, names)
-    idea_count = Idea.all().count()
-    idea_list = Idea.all().order('-created').fetch(100)
+    # Save new name ideas from file upload.
+    upload_names_form = UploadNamesForm(
+        request.POST if 'upload_file' in request.POST else None,
+        request.FILES if 'upload' in request.FILES else None)
+    if 'upload' in request.FILES:
+        upload = request.FILES['upload']
+        # logging.debug("Content-Length: %d", len(upload['content']))
+        # logging.debug("Content-Type: %s", upload['content-type'])
+        names = upload.read().split()
+        selected = [random.choice(names) for index in range(100)]
+        logging.debug(selected[:10])
+        return create_ideas(request, selected)
+    # Display list of recent names.
+    idea_list = Idea.all().order('-created').fetch(20)
     return render_to_response(request, 'names/index.html', locals())
 
 
@@ -32,5 +52,7 @@ def detail(request, key_name):
 
 def create_ideas(request, names):
     for name in names:
+        if '.' in name: # Cut off the top level domain.
+            name = name[:name.index('.')]
         idea = Idea.get_or_insert(key_name=name)
     return HttpResponseRedirect(request.path)
