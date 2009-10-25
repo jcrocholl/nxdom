a55dd15dde2cb6a9da0c0ca67ceb9f02d8d9e444
Johann C. Rocholl <johann@rocholl.net>
2009-10-25 06:35:41 -0700

    Added search form and score.

--- search/views.py
+++ search/views.py
@@ -10,7 +10,23 @@ class SearchForm(forms.Form):
         max_length=40,
         widget=forms.TextInput(attrs={'class': 'text span-4 focus'}))
     position = forms.ChoiceField(
-        choices=[('left', 'left'), ('right', 'right')])
+        choices=[('left', 'left'), ('right', 'right')],
+        widget=forms.RadioSelect(attrs={'class': 'radio'}))
+    length = forms.IntegerField(
+        initial=-1,
+        widget=forms.TextInput(attrs={'class': 'text span-2'}))
+    letters = forms.IntegerField(
+        initial=0,
+        widget=forms.TextInput(attrs={'class': 'text span-2'}))
+    digits = forms.IntegerField(
+        initial=-5,
+        widget=forms.TextInput(attrs={'class': 'text span-2'}))
+    dashes = forms.IntegerField(
+        initial=-10,
+        widget=forms.TextInput(attrs={'class': 'text span-2'}))
+    syllables = forms.IntegerField(
+        initial=0,
+        widget=forms.TextInput(attrs={'class': 'text span-2'}))
 
 
 def index(request):
@@ -27,4 +43,14 @@ def index(request):
         next = backwards[:-1] + chr(ord(backwards[-1]) + 1)
         idea_list.filter('backwards >=', backwards)
         idea_list.filter('backwards <', next)
+    if search_form.is_valid():
+        score_idea_list = []
+        for idea in idea_list.fetch(100):
+            score = 0
+            score += search_form.cleaned_data['length'] * idea.length
+            score += search_form.cleaned_data['letters'] * idea.letters
+            score += search_form.cleaned_data['digits'] * idea.digits
+            score += search_form.cleaned_data['dashes'] * idea.dashes
+            score_idea_list.append((score, idea))
+        score_idea_list.sort(reverse=True)
     return render_to_response(request, 'search/index.html', locals())
