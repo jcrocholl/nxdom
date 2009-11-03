5a9476058901c29a27aa21c6d0909bb8d26d5408
Johann C. Rocholl <johann@rocholl.net>
2009-11-02 23:02:52 -0800

    Only output words with specified length.

--- tools/short_names.py
+++ tools/short_names.py
@@ -6,11 +6,14 @@ sys.path[0] = os.path.dirname(sys.path[0])
 
 from dictionaries import english
 
+word_length = int(sys.argv[1])
 short = [word for word in english.SCOWL10 if len(word) <= 5]
 short.sort()
 for word1 in short:
-    print word1
+    if len(word1) == word_length:
+        print word1
+        continue
     for word2 in short:
         combo = word1 + word2
-        if len(combo) <= 9:
+        if len(combo) == word_length:
             print combo
