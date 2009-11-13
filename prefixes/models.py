7c04228790a5003f8f879c0c5ddd574cc52ce0f8
Johann C. Rocholl <johann@rocholl.net>
2009-11-13 14:59:35 -0800

    Removed inefficient counting for dot com prefixes.

--- prefixes/models.py
+++ prefixes/models.py
@@ -21,23 +21,6 @@ def count_domains(field, value):
         previous = keys[-1]
 
 
-def count_domains_dot_com(field, value):
-    result = 0
-    previous = None
-    while True:
-        domains = Domain.all()
-        domains.order('com').filter('com !=', None)
-        domains.filter(field, value)
-        domains.order('__key__')
-        if previous:
-            domains.filter('__key__ >', previous)
-        domains = domains.fetch(1000)
-        result += len(domains)
-        if len(domains) < 1000:
-            return result
-        previous = domains[-1].key()
-
-
 class Prefix(db.Model):
     """
     Count all domains in the datastore that start with the same prefix
@@ -79,12 +62,6 @@ class DotComPrefix(db.Model):
     count = db.IntegerProperty(required=True, default=0)
     timestamp = db.DateTimeProperty()
 
-    def count_domains(self):
-        name = self.key().name()
-        field = 'left%d' % len(name)
-        self.count = count_domains_dot_com(field, name)
-        self.timestamp = datetime.now()
-
 
 class DotComSuffix(db.Model):
     """
@@ -94,9 +71,3 @@ class DotComSuffix(db.Model):
     length = db.IntegerProperty(required=True)
     count = db.IntegerProperty(required=True, default=0)
     timestamp = db.DateTimeProperty()
-
-    def count_domains(self):
-        name = self.key().name()
-        field = 'right%d' % len(name)
-        self.count = count_domains_dot_com(field, name)
-        self.timestamp = datetime.now()
