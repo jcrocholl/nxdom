5895555173c4cca5acee0d3fb7a2435abfc5dc37
Johann C. Rocholl <johann@rocholl.net>
2009-11-13 20:00:28 -0800

    Fetch the most popular prefixes and suffixes.

---
+++ tools/popular_prefixes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Setup project environment in the parent directory.
+import os
+import sys
+sys.path[0] = os.path.dirname(sys.path[0])
+from common.appenginepatch.aecmd import setup_env
+setup_env()
+
+from google.appengine.ext import db
+from google.appengine.ext.remote_api import remote_api_stub
+from google.appengine.api.datastore_errors import Timeout
+
+from prefixes.models import DotComPrefix, DotComSuffix
+
+
+def auth_func():
+    return open('.passwd').read().split(':')
+
+
+def most_popular(query):
+    query.order('-count')
+    for prefix in query.fetch(10):
+        print prefix.count, prefix.key().name(),
+    print
+
+
+def main():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('--server', metavar='<hostname>',
+                      default='scoretool.appspot.com',
+                      help="connect to a different server")
+    (options, args) = parser.parse_args()
+    remote_api_stub.ConfigureRemoteDatastore(
+        'scoretool', '/remote_api', auth_func, options.server)
+    print 'Most popular prefixes:'
+    for length in range(3, 7):
+        most_popular(DotComPrefix.all().filter('length', length))
+    print 'Most popular suffixes:'
+    for length in range(3, 7):
+        most_popular(DotComSuffix.all().filter('length', length))
+
+
+if __name__ == '__main__':
+    main()
