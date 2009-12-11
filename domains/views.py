c3bf6288964a94d315ce851f062557c10a5bf51d
Johann C. Rocholl <johann@rocholl.net>
2009-12-10 17:50:05 -0800

    Log corrupt index results in the datastore for debugging.

--- domains/views.py
+++ domains/views.py
@@ -1,8 +1,9 @@
 import random
+import time
 from datetime import datetime
 
 from google.appengine.ext import db
-from google.appengine.ext.db import stats
+from google.appengine.ext.db import stats, GqlQuery
 
 from django.http import HttpResponseRedirect
 
@@ -12,6 +13,7 @@ from ragendja.dbutils import get_object_or_404
 from domains.models import MAX_NAME_LENGTH, DOMAIN_CHARS, OBSOLETE_ATTRIBUTES
 from domains.models import Domain
 from prefixes.selectors import Selector, random_name
+from indexes.models import Comparison
 
 BATCH_SIZE_FETCH = 100
 BATCH_SIZE_UPDATE = 100
@@ -79,22 +81,79 @@ def cron(request):
     return render_to_response(request, 'domains/index.html', locals())
 
 
+def color(result, trunc1, trunc2):
+    colored = []
+    for name in result:
+        if name not in trunc1:
+            colored.append('<span style="color:gray">%s</span>' % name)
+        elif name not in trunc2:
+            colored.append('<span style="color:red">%s</span>' % name)
+        else:
+            colored.append(name)
+    return colored
+
+
 def descending(request):
+    comparison = Comparison(path=request.META.get('PATH_INFO', ''),
+                            message="error",
+                            timestamp=datetime.now())
+    comparison.put()
     start_name = request.GET.get('start', random_name())
-    query_ascending = Domain.all(keys_only=True).order('__key__').filter(
-        '__key__ >=', db.Key.from_path('domains_domain', start_name))
-    names_ascending = [key.name() for key in query_ascending.fetch(100)]
-    query_descending = Domain.all(keys_only=True).order('-__key__').filter(
-        '__key__ <=', db.Key.from_path('domains_domain', names_ascending[-1]))
-    names_descending = [key.name() for key in query_descending.fetch(100)]
-    names_descending.reverse()
-    ascending = [('black' if name in names_descending else 'red', name)
-                 for name in names_ascending]
-    descending = [('black' if name in names_ascending else 'red', name)
-                  for name in names_descending]
-    missing = sum([int(name not in names_ascending)
-                   for name in names_descending])
-    percent_missing = 100 * missing / len(names_descending)
+    # Build and execute query 1.
+    gql1 = ' '.join(("SELECT __key__ FROM domains_domain",
+                     "WHERE __key__ >= :1 ORDER BY __key__ ASC",
+                     "LIMIT 100"))
+    key1 = db.Key.from_path('domains_domain', start_name)
+    comparison.gql1 = gql1.replace(':1', repr(key1))
+    start_time = time.time()
+    keys1 = GqlQuery(gql1, key1).fetch(100)
+    comparison.seconds1 = time.time() - start_time
+    result1 = [key.name() for key in keys1]
+    comparison.result1 = ' '.join(result1)
+    # Build and execute query 2.
+    gql2 = ' '.join(("SELECT __key__ FROM domains_domain",
+                     "WHERE __key__ <= :1 ORDER BY __key__ DESC",
+                     "LIMIT 100"))
+    key2 = db.Key.from_path('domains_domain', result1[-1])
+    comparison.gql2 = gql2.replace(':1', repr(key2))
+    start_time = time.time()
+    keys1 = GqlQuery(gql2, key2).fetch(100)
+    comparison.seconds2 = time.time() - start_time
+    result2 = [key.name() for key in keys1]
+    comparison.result2 = ' '.join(result2)
+    # Check sort order.
+    message = []
+    if sorted(result1) != result1:
+        message.append("query 1 returned incorrect sort order")
+    if sorted(result2, reverse=True) != result2:
+        message.append("query 2 returned incorrect sort order")
+    # Truncate result lists if necessary.
+    trunc1 = result1[:]
+    trunc2 = result2[:]
+    if trunc1 and trunc2 and trunc2[-1] < trunc1[0]:
+        while trunc1 and trunc2 and trunc2[-1] < trunc1[0]:
+            del trunc2[-1]
+    elif trunc1 and trunc2 and trunc1[0] < trunc2[-1]:
+        while trunc1 and trunc2 and trunc1[0] < trunc2[-1]:
+            del trunc1[0]
+    comparison.trunc1 = ' '.join(trunc1)
+    comparison.trunc2 = ' '.join(trunc2)
+    # Count missing entries.
+    set1 = set(trunc1)
+    set2 = set(trunc2)
+    comparison.missing1 = sum([int(name not in set1) for name in trunc2])
+    comparison.missing2 = sum([int(name not in set2) for name in trunc1])
+    # Human-readable error message.
+    if comparison.missing1:
+        message.append("query 1 missed %d items" % comparison.missing1)
+    if comparison.missing2:
+        message.append("query 2 missed %d items" % comparison.missing2)
+    # Save the results.
+    comparison.message = ' and '.join(message)
+    comparison.put()
+    # User-friendly HTML output.
     next_random_name = random_name()
     refresh_seconds = request.GET.get('refresh', 0)
+    colored1 = color(result1, trunc1, trunc2)
+    colored2 = color(result2, trunc2, trunc1)
     return render_to_response(request, 'domains/descending.html', locals())
---
+++ indexes/models.py
@@ -0,0 +1,19 @@
+from google.appengine.ext import db
+
+
+class Comparison(db.Model):
+    path = db.StringProperty()
+    message = db.StringProperty()
+    timestamp = db.DateTimeProperty()
+    # Query 1 and results.
+    gql1 = db.StringProperty()
+    seconds1 = db.FloatProperty()
+    result1 = db.TextProperty()
+    trunc1 = db.TextProperty()
+    missing1 = db.IntegerProperty()
+    # Query 2 and results.
+    gql2 = db.StringProperty()
+    seconds2 = db.FloatProperty()
+    result2 = db.TextProperty()
+    trunc2 = db.TextProperty()
+    missing2 = db.IntegerProperty()
