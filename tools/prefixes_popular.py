0e669d295927d42c118e5c1ecd95540e23397bc1
Johann C. Rocholl <johann@rocholl.net>
2009-12-22 11:19:01 -0800

    Updated popular prefixes and added score functions.

--- tools/prefixes_popular.py
+++ tools/prefixes_popular.py
@@ -7,33 +7,78 @@ sys.path[0] = os.path.dirname(sys.path[0])
 from common.appenginepatch.aecmd import setup_env
 setup_env()
 
+from pprint import pprint
+
 from google.appengine.ext import db
 from google.appengine.ext.remote_api import remote_api_stub
 from google.appengine.api.datastore_errors import Timeout
 
 from prefixes.models import DotComPrefix, DotComSuffix
+from utils.retry import retry
+
+LENGTHS = range(3, 10)
 
 
 def auth_func():
     return open('.passwd').read().split(':')
 
 
-def most_popular(query, variable):
+def most_popular(model, variable, length, maximums, totals):
+    query = model.all()
+    query.filter('length', length),
     query.order('-count')
-    total = 0
+    total = maximum = 0
     counters = []
-    for prefix in query.fetch(1000):
+    prefixes = retry(query.fetch, 1000)
+    for prefix in prefixes:
         if prefix.count < 10:
             break
         counters.append((prefix.key().name(), prefix.count))
+        if prefix.count > maximum:
+            maximum = prefix.count
         total += prefix.count
-    print variable + '_TOTAL =', total
-    counters.sort()
-    print variable, '= ['
+    counters.sort(key=lambda pair: -pair[1])
+    print '%s[%d] = {' % (variable, length)
     for pair in counters:
-        print "('%s', %d)," % pair
-    print ']'
-    print
+        print "'%s': %d," % pair
+    print '}'
+    totals[length] = int(total)
+    maximums[length] = int(maximum)
+
+
+def all_lengths(model, variable, lengths):
+    # Fetch and format popular prefixes.
+    totals = {}
+    maximums = {}
+    print '%s_SCORES = {}' % variable
+    for length in lengths:
+        most_popular(model, '%s_SCORES' % variable,
+                     length, maximums, totals)
+    print '%s_MAXIMUMS = ' % variable,
+    pprint(maximums)
+    print '%s_TOTALS = ' % variable,
+    pprint(totals)
+
+
+def score_function(variable, slice):
+    func = """
+
+def prefix_score(name):
+    best_score = 0.0
+    best_prefix = ''
+    for length in range(3, min(10, len(name) + 1)):
+        prefix = name[slice]
+        score = PREFIX_SCORES[length].get(prefix, 0)
+        score /= float(PREFIX_MAXIMUMS[length])
+        if score > best_score:
+            best_score = score
+            best_prefix = prefix
+    return best_score, best_prefix
+"""
+    func = func.replace('PREFIX', variable)
+    func = func.replace('prefix', variable.lower())
+    func = func.replace('[slice]', slice)
+    print func.rstrip()
 
 
 def main():
@@ -45,14 +90,11 @@ def main():
     (options, args) = parser.parse_args()
     remote_api_stub.ConfigureRemoteDatastore(
         'scoretool', '/remote_api_hidden', auth_func, options.server)
-    print 'PREFIX_SCORES = {}'
-    for length in range(3, 11):
-        most_popular(DotComPrefix.all().filter('length', length),
-                     'LEFT%d' % length)
-    print 'SUFFIX_SCORES = {}'
-    for length in range(3, 11):
-        most_popular(DotComSuffix.all().filter('length', length),
-                     'RIGHT%d' % length)
+    all_lengths(DotComPrefix, 'PREFIX', LENGTHS)
+    print
+    all_lengths(DotComSuffix, 'SUFFIX', LENGTHS)
+    score_function('PREFIX', '[:length]')
+    score_function('SUFFIX', '[-length:]')
 
 
 if __name__ == '__main__':
