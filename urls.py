a63b9ab865273262dd5ea1ff919b12d9c742ec86
Johann C. Rocholl <johann@rocholl.net>
2010-01-01 05:48:22 -0800

    Added static pages: about, faq, terms, privacy.

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
-from ragendja.urlsauto import urlpatterns
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+
+from ragendja.urlsauto import urlpatterns
 
 admin.autodiscover()
 
@@ -8,5 +10,9 @@ handler500 = 'ragendja.views.server_error'
 
 urlpatterns = patterns('',
     ('^admin/(.*)', admin.site.root),
+    ('^about/$', direct_to_template, {'template': 'about.html'}),
+    ('^faq/$', direct_to_template, {'template': 'faq.html'}),
+    ('^terms/$', direct_to_template, {'template': 'terms.html'}),
+    ('^privacy/$', direct_to_template, {'template': 'privacy.html'}),
     (r'^$', 'search.views.index'),
 ) + urlpatterns
