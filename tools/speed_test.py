f2a1c0347200f0a897a31707a3d3e4e7cfdabaea
Johann C. Rocholl <johann@rocholl.net>
2010-01-12 11:13:52 -0800

    Simple script to measure page load time.

---
+++ tools/speed_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import urllib2
+
+
+def milliseconds(url):
+    start = time.time()
+    html = urllib2.urlopen(url).read()
+    finish = time.time()
+    return int(1000 * (finish - start))
+
+
+def main():
+    url = 'http://www.nxdom.com/'
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    results = [milliseconds(url) for i in range(14)]
+    results.sort()
+    print time.strftime('%Y-%m-%d %H:%M:%S'),
+    print ' '.join([str(i) for i in results])
+
+
+if __name__ == '__main__':
+    main()
