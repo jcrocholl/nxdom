a993ecec7442e14d5c29b5077ebdb1ca1fc9e17a
Johann C. Rocholl <johann@rocholl.net>
2010-01-15 00:38:09 -0800

    Accept Etag and Last-Modified headers for speedup.

--- tools/speed_test.py
+++ tools/speed_test.py
@@ -4,22 +4,45 @@ import sys
 import time
 import urllib2
 
+NUM_REQUESTS = 20
+
+etag = None
+last_modified = None
+
 
 def milliseconds(url):
+    info = ''
+    request = urllib2.Request(url)
+    global etag, last_modified
+    if etag:
+        request.add_header('If-None-Match', etag)
+    if last_modified:
+        request.add_header('If-Modified-Since', last_modified)
     start = time.time()
-    html = urllib2.urlopen(url).read()
+    try:
+        response = urllib2.urlopen(request)
+        if 'Etag' in response.headers:
+            etag = response.headers['etag']
+        if 'Last-Modified' in response.headers:
+            last_modified = response.headers['Last-Modified']
+        html = response.read()
+    except urllib2.HTTPError, status:
+        if status.code == 304:
+            info = '='
+        else:
+            raise
     finish = time.time()
-    return int(1000 * (finish - start))
+    return (1000 * (finish - start), info)
 
 
 def main():
     url = 'http://www.nxdom.com/'
     if len(sys.argv) > 1:
         url = sys.argv[1]
-    results = [milliseconds(url) for i in range(10)]
+    results = [milliseconds(url) for i in range(NUM_REQUESTS)]
     results.sort()
     print time.strftime('%Y-%m-%d %H:%M:%S'),
-    print ' '.join([str(i) for i in results])
+    print ' '.join(['%d%s' % pair for pair in results])
 
 
 if __name__ == '__main__':
