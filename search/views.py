46e4f33f1ecddb4ed6f8c6ba05b543150cf7d22c
Johann C. Rocholl <johann@rocholl.net>
2010-02-26 00:17:50 -0800

    Fixed server error for domains without lookups. Also removed obsolete score_domains function.

--- search/views.py
+++ search/views.py
@@ -90,36 +90,6 @@ def index(request):
     return render_to_response(request, 'search/index.html', locals())
 
 
-def score_domains(cleaned_data, domain_list):
-    score_domain_list = []
-    for domain in domain_list:
-        score = 0
-        # Character counts.
-        score += domain.length * cleaned_data['len']
-        score += domain.digits * cleaned_data['digits']
-        score += domain.dashes * cleaned_data['dashes']
-        # Language scores.
-        if domain.language_scores_need_update():
-            domain.update_language_scores()
-        score += domain.english * cleaned_data['english']
-        score += domain.spanish * cleaned_data['spanish']
-        score += domain.french * cleaned_data['french']
-        score += domain.german * cleaned_data['german']
-        # Popular prefixes and suffixes.
-        if domain.popularity_scores_need_update():
-            domain.update_popularity_scores()
-        score += domain.prefix * cleaned_data['prefix']
-        score += domain.suffix * cleaned_data['suffix']
-        # Available domain names.
-        for tld in TOP_LEVEL_DOMAINS:
-            if hasattr(domain, tld) and getattr(domain, tld) == 0:
-                score += cleaned_data[tld]
-        domain.weighted_score = score
-    domain_list.sort(
-        key=lambda domain: (-domain.weighted_score, domain.key().name()))
-    return domain_list[:50]
-
-
 def best_left_right(left, right):
     if left and not right:
         left = left[:6]
@@ -160,9 +130,12 @@ def fetch_dns_lookups(domains):
     keys = [db.Key.from_path('dns_lookup', domain.key().name())
             for domain in domains]
     lookups = db.get(keys)
-    for domain, lookup in zip(domains, lookups):
+    for index in range(len(domains) - 1, -1, -1):
+        lookup = lookups[index]
         if not lookup:
+            domains.pop(index)
             continue
+        domain = domains[index]
         domain.updated = lookup.timestamp
         for tld in TOP_LEVEL_DOMAINS:
             value = getattr(lookup, tld, None)
